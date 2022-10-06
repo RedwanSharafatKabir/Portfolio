7296e1cadad49be3116c22f6847efb68c71dce7c
Redwan Sharafat Kabir <37416018+RedwanSharafatKabir@users.noreply.github.com>
2022-10-07 01:41:29 +0600

    Portfolio

---
+++ Updated portfolio/js/script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottieId'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets6.lottiefiles.com/packages/lf20_lvgmrl5j.json'
+  })
